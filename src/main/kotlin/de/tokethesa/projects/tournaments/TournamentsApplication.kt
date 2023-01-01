c4dbf58b572908b1ca4a729568609bbe8b66c0aa
Tobias Hein <tobi.hein@tokethesa.de>
2023-01-01 22:42:54 +0100

    Initial spring boot project

---
+++ src/main/kotlin/de/tokethesa/projects/tournaments/TournamentsApplication.kt
@@ -0,0 +1,11 @@
+package de.tokethesa.projects.tournaments
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TournamentsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TournamentsApplication>(*args)
+}
