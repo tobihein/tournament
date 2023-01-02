4d2946185034e175f195ef58ec98666bab6c3174
Tobias Hein <tobi.hein@tokethesa.de>
2023-01-02 16:26:40 +0100

    Empty teams component

--- frontend/tournament-app/src/app/app.module.ts
+++ frontend/tournament-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeamsComponent } from './components/teams/teams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule
